28ac9286cd97e47ee15a879e4293af57e7250e5b
Ozodbek <ilyosovozodbek7@gmail.com>
2023-04-02 14:13:51 +0500

    4th page

--- components/TheSolution.vue
+++ components/TheSolution.vue
@@ -1,13 +1,84 @@
 <template>
-    
+  <!-- SECTION RENTAL START -->
+  <section class="rental">
+    <div class="container">
+      <div class="rental-text">
+        <p class="rental-main">Проектный решения</p>
+        <p class="little">
+          Уважаемая госпадин, наша компания ООО «AVP Rental Group UC»
+          приветствует Вас, мы оказываем услуги и производи работы , по <br />
+          свайному бурению скважин различного диаметра. <br />
+          Мы обладаем современным 12шт буровым оборудованием для выполнения
+          широкого круга работ. Занимаемся базовым инженерным строительством,
+          примерно: <br />
+          --- буронабивными сваями; <br />
+          ---сборными сваями статического давления(забивания); <br />
+          --- армированием различных слабых грунтов; <br />
+          --- строительством и мониторингом ограждающих конструкций подземного
+          пространства глубоких котлованов.
+        </p>
+        <img class="rental-img" src="../assets/img/rental-main.svg" alt="" />
+      </div>
+    </div>
+  </section>
+  <!-- SECTION RENTAL END -->
+
+  <!-- SECTION PROJECT-SERVICE START -->
+  <section class="project-service">
+    <div class="container">
+      <div class="a-top">
+        <div class="a-text">Услуги</div>
+        <div class="a-line"></div>
+      </div>
+
+      <div class="block">
+        <img
+          class="block-img"
+          src="../assets/img/project-1st.svg"
+          alt="There is a photo of company services"
+        />
+        <div class="mini-block">
+          <p class="main-text">Услуги по бурению</p>
+          <p class="mini-text">
+            --- буронабивными сваями; <br />
+            ---сборными сваями статического давления(забивания);<br />
+            --- армированием различных слабых грунтов; <br />
+            --- строительством и мониторингом ограждающих конструкций подземного
+            пространства глубоких котлованов
+          </p>
+        </div>
+      </div>
+      <div class="block">
+        <img
+          class="block-img"
+          src="../assets/img/project-2nd.svg"
+          alt="There is a photo of company services"
+        />
+        <div class="mini-block">
+          <p class="main-text">Аренда услуг</p>
+          <p class="mini-text">Аренда оборудования используя технику.</p>
+        </div>
+      </div>
+      <div class="block">
+        <img
+          class="block-img"
+          src="../assets/img/project-3rd.svg"
+          alt="There is a photo of company services"
+        />
+        <div class="mini-block">
+          <p class="main-text">Продажа оборудования</p>
+          <p class="mini-text">
+            Покупайте буровое оборудование и облегчайте себе работу
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- SECTION PROJECT-SERVICE END -->
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
