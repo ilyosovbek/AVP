3daa5ea2100c6b72f1d30c29cfdcf3ac36870000
Khayrullo <xakimovxayrullo91@gmail.com>
2023-04-09 07:47:54 +0500

    burger

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,33 +1,37 @@
 <template>
     <header>
         <div class="container">
-            <div class="navbar" :class="isOpen ? 'open' : ''">
-                <img src="../assets/img/logo.svg" class="img" alt="logo">
-                <ul class="menu">
-                    <li>
-                        <NuxtLink class="list" to="/">Главные</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink class="list" to="/techniques">Наши техники</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink class="list" to="/services">Услуги /аренда</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink class="list" to="/solution">Проектные решения</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink class="list" to="/projects">Проекты</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink class="list" to="/about">О нас</NuxtLink>
-                    </li>
-                </ul>
-                <p>
-                    <NuxtLink class="contact" to="/contact">Связаться с нами</NuxtLink>
-                </p>
+            <div class="wrapper" :class="{isOpen}">
+                <div class="navbar">
+                    <img src="../assets/img/logo.svg" class="img" alt="logo">
+                    <ul class="menu">
+                        <li>
+                            <NuxtLink class="list" to="/">Главные</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink class="list" to="/techniques">Наши техники</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink class="list" to="/services">Услуги /аренда</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink class="list" to="/solution">Проектные решения</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink class="list" to="/projects">Проекты</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink class="list" to="/about">О нас</NuxtLink>
+                        </li>
+                    </ul>
+                    <p>
+                        <NuxtLink class="contact" to="/contact">Связаться с нами</NuxtLink>
+                    </p>
+
+
+                </div>
                 <!-- MENU OPEN -->
-                <div class="burger">
+                <div class="burger"  :class="{isOpen}">
                     <img src="../assets/img/logo.svg" class="img" alt="logo">
                     <ul class="bg-menu">
                         <li>
@@ -54,7 +58,7 @@
                     </p>
                 </div>
                 <!-- BURGER MENU -->
-                <div class="top-hd__brg">
+                <div class="top-hd__brg" @click="isOpen = !isOpen">
                     <div class="line-1"></div>
                     <div class="line-2"></div>
                     <div class="line-3"></div>
@@ -66,9 +70,9 @@
 <script>
 
 export default {
-    data(){
-        return{
-            isOpen:false,
+    data() {
+        return {
+            isOpen: false,
         }
     }
 };
