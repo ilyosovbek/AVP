e63361393346bf3213225cb9d3208e6d65e6c686
Khayrullo <xakimovxayrullo91@gmail.com>
2023-04-15 14:54:03 +0500

    ddd

--- components/TheRental.vue
+++ components/TheRental.vue
@@ -6,64 +6,26 @@
         <div class="a-line"></div>
       </div>
       <div class="block">
-        <img
-          class="rental-img"
-          src="../assets/img/rental-1st.svg"
-          alt="There is a photo of company technologies"
-        />
-        <div class="mini-block">
-          <div class="extra-block">
-            <img
-              class="img"
-              src="../assets/img/rental-extra-1.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-2.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-3.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-4.svg"
-              alt="There is a logo of equipment brands"
-            />
-          </div>
-          <div class="info-block">
-            <div class="top-text">Автокран</div>
-            <div class="btm-text">Вам нужен автокран?</div>
-            <a class="info-link" href="">See More</a>
-          </div>
+        <img class="rental-img" src="../assets/img/rental-1st.svg" alt="There is a photo of company technologies" />
+        <div class="extra-block">
+          <img class="img" src="../assets/img/rental-extra-1.svg" alt="There is a logo of equipment brands" />
+          <img class="img" src="../assets/img/rental-extra-2.svg" alt="There is a logo of equipment brands" />
+          <img class="img" src="../assets/img/rental-extra-3.svg" alt="There is a logo of equipment brands" />
+          <img class="img" src="../assets/img/rental-extra-4.svg" alt="There is a logo of equipment brands" />
+        </div>
+        <div class="info-block">
+          <p class="top-text">Автокран</p>
+          <p class="btm-text">Вам нужен автокран?</p>
+          <a class="info-link" href="#!">See More</a>
         </div>
       </div>
       <div class="block">
         <div class="mini-block">
           <div class="extra-blocky">
-            <img
-              class="img"
-              src="../assets/img/rental-extra-1.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-2.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-3.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-4.svg"
-              alt="There is a logo of equipment brands"
-            />
+            <img class="img" src="../assets/img/rental-extra-1.svg" alt="There is a logo of equipment brands" />
+            <img class="img" src="../assets/img/rental-extra-2.svg" alt="There is a logo of equipment brands" />
+            <img class="img" src="../assets/img/rental-extra-3.svg" alt="There is a logo of equipment brands" />
+            <img class="img" src="../assets/img/rental-extra-4.svg" alt="There is a logo of equipment brands" />
           </div>
           <div class="info-block">
             <div class="top-text">Автокран</div>
@@ -71,41 +33,17 @@
             <a class="info-link" href="">See More</a>
           </div>
         </div>
-        <img
-          class="rental-image"
-          src="../assets/img/rental-2nd.svg"
-          alt="There is a photo of company technologies"
-        />
+        <img class="rental-image" src="../assets/img/rental-2nd.svg" alt="There is a photo of company technologies" />
       </div>
 
       <div class="block">
-        <img
-          class="rental-img"
-          src="../assets/img/rental-3rd.svg"
-          alt="There is a photo of company technologies"
-        />
+        <img class="rental-img" src="../assets/img/rental-3rd.svg" alt="There is a photo of company technologies" />
         <div class="mini-block">
           <div class="extra-blocks">
-            <img
-              class="img"
-              src="../assets/img/rental-extra-1.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-2.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-3.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-4.svg"
-              alt="There is a logo of equipment brands"
-            />
+            <img class="img" src="../assets/img/rental-extra-1.svg" alt="There is a logo of equipment brands" />
+            <img class="img" src="../assets/img/rental-extra-2.svg" alt="There is a logo of equipment brands" />
+            <img class="img" src="../assets/img/rental-extra-3.svg" alt="There is a logo of equipment brands" />
+            <img class="img" src="../assets/img/rental-extra-4.svg" alt="There is a logo of equipment brands" />
           </div>
           <div class="info-block">
             <div class="top-text">Автокран</div>
@@ -118,26 +56,10 @@
       <div class="block">
         <div class="mini-block">
           <div class="extra-blocky">
-            <img
-              class="img"
-              src="../assets/img/rental-extra-1.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-2.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-3.svg"
-              alt="There is a logo of equipment brands"
-            />
-            <img
-              class="img"
-              src="../assets/img/rental-extra-4.svg"
-              alt="There is a logo of equipment brands"
-            />
+            <img class="img" src="../assets/img/rental-extra-1.svg" alt="There is a logo of equipment brands" />
+            <img class="img" src="../assets/img/rental-extra-2.svg" alt="There is a logo of equipment brands" />
+            <img class="img" src="../assets/img/rental-extra-3.svg" alt="There is a logo of equipment brands" />
+            <img class="img" src="../assets/img/rental-extra-4.svg" alt="There is a logo of equipment brands" />
           </div>
           <div class="info-block">
             <div class="top-text">Автокран</div>
@@ -145,11 +67,7 @@
             <a class="info-link" href="">See More</a>
           </div>
         </div>
-        <img
-          class="rental-image"
-          src="../assets/img/rental-4th.svg"
-          alt="There is a photo of company technologies"
-        />
+        <img class="rental-image" src="../assets/img/rental-4th.svg" alt="There is a photo of company technologies" />
       </div>
     </div>
   </section>
